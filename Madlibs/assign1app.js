b38add95b42570a3154a522219ed73edea7cf693
Brendan Montenegro <brendan.montenegro@f1006-29-34175.bmcc.cuny.edu>
2017-09-18 15:00:31 -0400

    add madlib

--- Madlibs/assign1app.js
+++ Madlibs/assign1app.js
@@ -6,4 +6,10 @@ var sentence = "<h2>My Company,  " + noun ;
 sentence += ' is developing ' +product ;
 sentence += ' to help ' +people +'. </h2';
 
+if (noun.value){
+    sentence = "<h2>My Company " + noun.value
+}
+else {
+		noun = "Please input a Company name.";
+	}
 document.write(sentence);
