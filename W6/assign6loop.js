81f0d37e660b1f208209dac33d518a0d0b435afb
montenegroo <2015bmontenegro10@gmail.com>
2017-10-23 13:27:52 -0400

    Add files via upload

--- W6/assign6loop.js
+++ W6/assign6loop.js
@@ -1,21 +1,21 @@
+var submit = document.getElementById("submit");
+submit.onclick = function () {
     var username = getElementById("username").value;
     var keyword = getElementById("keyword").value;
     var chars = "abcdefghijklmnopqrstuvwxyz123456789";
-    var pass= " ";
-    var submit = document.getElementById("submit"); 
+    var pass = " ";
     
     
 
-var generatePassword = function (){
-     for (i=1; i<=number; i++) {
+var pass= function generatePassword{
+     for ( i=1; i<=number; i++) {
         var length = keyword.length;
         var i = Math.floor(Math.random() * length);
         pass += keyword.charAt(i);
-    }
+    };
     return pass;
 };
-submit.onclick = function () {
+
     document.getElementById("password").innerText = pass;
-    
-    };
+
         
